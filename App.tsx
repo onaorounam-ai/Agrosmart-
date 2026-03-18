7e787157ef48880650b21ae3fc2b1aed27e34413
onaorounam-ai <onaorounam@gmail.com>
2026-03-18 15:07:27 +0100

    Delete App.tsx

--- App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: '50px', textAlign: 'center', fontFamily: 'sans-serif' }}>
-      <h1 style={{ color: '#2e7d32' }}>🌱 Agrosmart en Ligne !</h1>
-      <p>Si vous voyez ce message, c'est que la configuration est réussie.</p>
-    </div>
-  )
-}
